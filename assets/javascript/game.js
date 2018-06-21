9308444d2b2e1b034be21639f8b3a8dd9a53deb8
Philip S(D) <philipsteininger@gmail.com>
2018-06-21 16:21:16 -0700

    blank letters displayed, keyup registered

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,38 @@
 $(document).ready(function() {
-
-
-
-    // document.getElementById("guessedLetters").onclick = function addP() {
-    //     document.getElementById("guessedLetters").innerHTML += "P";
-    // }
-
-
-
-
-
-
-
     var hangman = {
-        wordList: [],
+        wordList: ["hello world"],
         currentWord: "",
         alphabet: "abcdefghijklmnopqrstuvwxyz",
         guessedLetters: "",
-
-        startGame: function () {
+        initializeGame: function () {
             this.currentWord = this.wordList[Math.floor(Math.random() * this.wordList.length)];
+            console.log(this.currentWord);
             this.guessedLetters = "";
-
+            for(var i = 0; i < this.currentWord.length; i++) {
+                if(this.currentWord[i] === " ") {
+                    $("#the-word").append("  ");
+                } else {
+                    $("#the-word").append("<span id=\"letter"+i+"\">-</span>");
+                }
+            }
         },
-        letterGuess: function () {
-            $("#guessedLetters").append("P");
+        letterGuess: function (guess) {
+            console.log(guess.key);
+            //this.currentWord.search(guess.key);
+            console.log(this.currentWord.search(guess.key));
+            var j = this.currentWord.search(guess.key);
+            console.log($("#letter"+j));
         },
         endGame: function () {
 
         }
 
     };
-    $("#guessedLetters").click(hangman.letterGuess);
 
-
-    //     function letterGuess() {
-    //         var LETTERS = document.getElementById("guessedLetters");
-    //         LETTERS.innerHTML += "P";
-    //     }
-
-    
-
-    // window.onclick = letterGuess();
-    // }
-
-    function modifyText() {
-        var t2 = document.getElementById("t2");
-        if (t2.firstChild.nodeValue == "three") {
-          t2.firstChild.nodeValue = "two";
-        } else {
-          t2.firstChild.nodeValue = "three";
-        }
-      }
-      
-      // add event listener to table
-      var el = document.getElementById("outside");
-      el.addEventListener("click", modifyText, false);
-
+    hangman.initializeGame();
+    $(document).on("keyup", function(e) {
+        hangman.letterGuess(e);
+    });
+    $("#guessed-letters").click(hangman.letterGuess);
+    $("")
 });
