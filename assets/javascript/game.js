52766d054eb7b7de6d712d395d138a9922da9f9f
Philip S(D) <philipsteininger@gmail.com>
2018-06-22 13:10:44 -0700

    records wins and losses, restart button, some styling added.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,87 @@
 $(document).ready(function () {
+
     var hangman = {
         wordList: ["hello worldl"],
         currentWord: "",
         alphabet: "abcdefghijklmnopqrstuvwxyz",
         guessedLetters: "",
+        initialGuesses: 5,
+        guessesLeft: 0,
+        wins: 0,
+        losses: 0,
         initializeGame: function () {
+            $(document).off("keyup");
+            $(document).on("keyup", function (e) {
+                hangman.letterGuess(e);
+            });
+            $("#restart-button").click(function () {
+                $("#the-word").empty();
+                $("#msg-box").empty();
+                $("#guessed-letters").empty();
+                hangman.initializeGame();
+            });
+            $("#msg-box").text("press any letter to begin guessing.");
+            this.guessesLeft = this.initialGuesses;
+            $("#guesses-left").text(this.initialGuesses);
+            $("#wins-span").text(this.wins);
+            $("#losses-span").text(this.losses);
             this.currentWord = this.wordList[Math.floor(Math.random() * this.wordList.length)];
             console.log(this.currentWord);
             this.guessedLetters = "";
             for (var i = 0; i < this.currentWord.length; i++) {
                 if (this.currentWord[i] === " ") {
-                    $("#the-word").append("  ");
+                    $("#the-word").append(" ");
                 } else {
                     $("#the-word").append("<span id=\"letter" + i + "\">-</span>");
                 }
             }
         },
         letterGuess: function (guess) {
-            if (this.alphabet.search(guess.key) !== -1) {
-                if (this.guessedLetters.search(guess.key) !== -1) {
-                    console.log("already guessed");
+            if (guess.which >= 65 && guess.which <= 90) {
+                var guessKey = guess.key.toLowerCase();
+                if (this.guessedLetters.search(guessKey) !== -1) {
+                    $("#msg-box").text(guessKey + " has already been guessed.");
                 } else {
-                    this.guessedLetters += guess.key;
-                    var j = this.currentWord.search(guess.key);
+                    this.guessedLetters += guessKey;
+                    $("#guessed-letters").append("<span>" + guessKey + " </span>");
+                    var j = this.currentWord.search(guessKey);
                     if (j === -1) {
-                        console.log("miss");
+                        $("#msg-box").text("that's a miss.");
+                        this.guessesLeft--;
+                        $("#guesses-left").text(this.guessesLeft);
+                        if (this.guessesLeft === 0) {
+                            this.endGame(false);
+                        }
                     } else {
+                        $("#msg-box").text("that's a hit.");
                         while (j < this.currentWord.length) {
-                            if (this.currentWord[j] === guess.key) {
-                                $("#letter" + j).text(guess.key);
+                            if (this.currentWord[j] === guessKey) {
+                                $("#letter" + j).text(guessKey);
                             }
                             j++;
                         }
+                        if ($("#the-word").text() === this.currentWord) {
+                            this.endGame(true);
+                        }
                     }
                 }
+
             }
         },
-        endGame: function () {
-
+        endGame: function (isWin) {
+            if (isWin) {
+                this.wins++;
+                $("#msg-box").text("you win. yay.");
+                //$("#wins-span").text(this.wins);
+            } else {
+                this.losses++;
+                $("#msg-box").text("you lose. rip");
+                //$("#losses-span").text(this.losses);
+            }
+            $(document).off("keyup");
         }
 
     };
-
     hangman.initializeGame();
-    $(document).on("keyup", function (e) {
-        hangman.letterGuess(e);
-    });
-    $("#guessed-letters").click(hangman.letterGuess);
-    $("")
+
 });
