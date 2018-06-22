87e2342c2979785663b9f0c65498288fa40731e2
Philip S(L) <philipsteininger@gmail.com>
2018-06-21 18:29:07 -0700

    letters now pop up when guessed, registers already guessed letters and misses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var hangman = {
-        wordList: ["hello world"],
+        wordList: ["hello worldl"],
         currentWord: "",
         alphabet: "abcdefghijklmnopqrstuvwxyz",
         guessedLetters: "",
@@ -8,20 +8,33 @@ $(document).ready(function() {
             this.currentWord = this.wordList[Math.floor(Math.random() * this.wordList.length)];
             console.log(this.currentWord);
             this.guessedLetters = "";
-            for(var i = 0; i < this.currentWord.length; i++) {
-                if(this.currentWord[i] === " ") {
+            for (var i = 0; i < this.currentWord.length; i++) {
+                if (this.currentWord[i] === " ") {
                     $("#the-word").append("  ");
                 } else {
-                    $("#the-word").append("<span id=\"letter"+i+"\">-</span>");
+                    $("#the-word").append("<span id=\"letter" + i + "\">-</span>");
                 }
             }
         },
         letterGuess: function (guess) {
-            console.log(guess.key);
-            //this.currentWord.search(guess.key);
-            console.log(this.currentWord.search(guess.key));
-            var j = this.currentWord.search(guess.key);
-            console.log($("#letter"+j));
+            if (this.alphabet.search(guess.key) !== -1) {
+                if (this.guessedLetters.search(guess.key) !== -1) {
+                    console.log("already guessed");
+                } else {
+                    this.guessedLetters += guess.key;
+                    var j = this.currentWord.search(guess.key);
+                    if (j === -1) {
+                        console.log("miss");
+                    } else {
+                        while (j < this.currentWord.length) {
+                            if (this.currentWord[j] === guess.key) {
+                                $("#letter" + j).text(guess.key);
+                            }
+                            j++;
+                        }
+                    }
+                }
+            }
         },
         endGame: function () {
 
@@ -30,7 +43,7 @@ $(document).ready(function() {
     };
 
     hangman.initializeGame();
-    $(document).on("keyup", function(e) {
+    $(document).on("keyup", function (e) {
         hangman.letterGuess(e);
     });
     $("#guessed-letters").click(hangman.letterGuess);
